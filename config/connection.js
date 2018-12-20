bf34a5d16dd25c484503de3427c520a70fae8c79
William Vasquez <william.a.vasquez@gmail.com>
2018-12-19 19:16:03 -0500

    HeY Save next time

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 
 
 if(process.env.JAWSDB_URL){
-
+  var connection = mysql.createConnection(process.env.JAWSDB_URL)
 }else {
 var connection = mysql.createConnection({
   host: "localhost",
